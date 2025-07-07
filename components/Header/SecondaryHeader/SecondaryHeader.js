18f3ff4f89f5d156cf42826460f43360d5117aaf
DestinAsian <webmaster@destinasian.com>
2025-07-07 15:01:32 +0700

    modified-components/Header/SecondaryHeader/SecondaryHeader.js

--- components/Header/SecondaryHeader/SecondaryHeader.js
+++ components/Header/SecondaryHeader/SecondaryHeader.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useQuery } from '@apollo/client'
 import classNames from 'classnames/bind'
 import styles from './SecondaryHeader.module.scss'
 import dynamic from 'next/dynamic'
-
-const TravelGuidesMenu = dynamic(() =>
-  import('../../../components/TravelGuidesMenu/TravelGuidesMenu'),
-)
 import Link from 'next/link'
 import { GetSecondaryHeaders } from '../../../queries/GetSecondaryHeaders'
 
-let cx = classNames.bind(styles)
+const TravelGuidesMenu = dynamic(() =>
+  import('../../../components/TravelGuidesMenu/TravelGuidesMenu')
+)
+
+const cx = classNames.bind(styles)
 
 export default function SecondaryHeader({
   setSearchQuery,
@@ -18,13 +18,21 @@ export default function SecondaryHeader({
   setIsGuidesNavShown,
   isScrolled,
 }) {
-  const { data, error } = useQuery(GetSecondaryHeaders, {
+  // Query untuk mengambil kategori dinamis (misalnya: News, Insights, Features)
+  const { data, error, loading } = useQuery(GetSecondaryHeaders, {
     variables: { include: ['20', '29', '3'] },
   })
 
   if (error) return <div>Error loading categories!</div>
 
-  const categories = data?.categories?.edges || []
+  // Gunakan useMemo agar kategori tidak dihitung ulang kecuali data berubah
+  const categories = useMemo(() => data?.categories?.edges || [], [data])
+
+  // Fungsi toggle untuk guides navigation
+  const toggleGuidesNav = () => {
+    setIsGuidesNavShown((prev) => !prev)
+    setSearchQuery('')
+  }
 
   return (
     <>
@@ -35,20 +43,17 @@ export default function SecondaryHeader({
             className={cx('menu-button', 'menu-button-guides', {
               active: isGuidesNavShown,
             })}
-            onClick={() => {
-              setIsGuidesNavShown(!isGuidesNavShown)
-              setSearchQuery('')
-            }}
+            onClick={toggleGuidesNav}
             aria-label="Toggle navigation"
           >
-            <div className={cx('menu-title')}>{`Guides`}</div>
+            <div className={cx('menu-title')}>Guides</div>
           </button>
 
-          {/* Render kategori dinamis (News, Insights, Features) */}
-          {categories.map((category) => {
-            const { id, name, slug } = category.node
+          {/* Render kategori dinamis */}
+          {categories.map(({ node: category }) => {
+            const { id, name, uri } = category
             return (
-              <Link key={id} href={`/${slug}`}>
+              <Link key={id} href={`/${uri}`}>
                 <div className={cx('menu-button')}>
                   <div className={cx('menu-title')}>{name}</div>
                 </div>
@@ -58,12 +63,7 @@ export default function SecondaryHeader({
         </div>
       </div>
 
-      <div
-        className={cx(
-          'full-menu-content',
-          isGuidesNavShown ? 'show' : undefined,
-        )}
-      >
+      <div className={cx('full-menu-content', { show: isGuidesNavShown })}>
         <div className={cx('full-menu-wrapper')}>
           <TravelGuidesMenu />
         </div>
