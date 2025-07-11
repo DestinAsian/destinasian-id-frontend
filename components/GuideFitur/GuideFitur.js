308834e518feddbb7b97ba2f5655427185099f80
DestinAsian <webmaster@destinasian.com>
2025-07-11 17:16:53 +0700

    Improve layout and style for GuideFitur and GuideReelIg sections

--- components/GuideFitur/GuideFitur.js
+++ components/GuideFitur/GuideFitur.js
@@ -1,5 +1,61 @@
+// import React from 'react'
+// import styles from './GuideFitur.module.scss'
+
+// const GuideFitur = ({ guidesfitur }) => {
+//   if (!guidesfitur) return null
+
+//   const guideItems = [
+//     {
+//       title: guidesfitur.titleGuideFitur1,
+//       image: guidesfitur.featureImageGuideFitur1?.mediaItemUrl,
+//       url: guidesfitur.linkUrlGuideFitur1,
+//     },
+//     {
+//       title: guidesfitur.titleGuideFitur2,
+//       image: guidesfitur.featureImageGuideFitur2?.mediaItemUrl,
+//       url: guidesfitur.linkUrlGuideFitur2,
+//     },
+//     {
+//       title: guidesfitur.titleGuideFitur3,
+//       image: guidesfitur.featureImageGuideFitur3?.mediaItemUrl,
+//       url: guidesfitur.linkUrlGuideFitur3,
+//     },
+//     {
+//       title: guidesfitur.titleGuideFitur4,
+//       image: guidesfitur.featureImageGuideFitur4?.mediaItemUrl,
+//       url: guidesfitur.linkUrlGuideFitur4,
+//     },
+//   ]
+
+//   return (
+//     <div className={styles.guideFiturWrap}>
+//       {guideItems.map(
+//         (item, index) =>
+//           item.title &&
+//           item.image &&
+//           item.url && (
+//             <a
+//               key={index}
+//               href={item.url}
+//               className={styles.guideFiturItem}
+//             >
+//               <div className={styles.imageWrapper}>
+//                 <img src={item.image} alt={item.title} />
+//                 <div className={styles.titleOverlay}>{item.title}</div>
+//               </div>
+//             </a>
+//           ),
+//       )}
+//     </div>
+//   )
+// }
+
+// export default GuideFitur
+
+
 import React from 'react'
 import styles from './GuideFitur.module.scss'
+import classNames from 'classnames'
 
 const GuideFitur = ({ guidesfitur }) => {
   if (!guidesfitur) return null
@@ -25,27 +81,30 @@ const GuideFitur = ({ guidesfitur }) => {
       image: guidesfitur.featureImageGuideFitur4?.mediaItemUrl,
       url: guidesfitur.linkUrlGuideFitur4,
     },
-  ]
+  ].filter((item) => item.title && item.image && item.url) // Hanya item valid
+
+  const itemCount = guideItems.length
 
   return (
-    <div className={styles.guideFiturWrap}>
-      {guideItems.map(
-        (item, index) =>
-          item.title &&
-          item.image &&
-          item.url && (
-            <a
-              key={index}
-              href={item.url}
-              className={styles.guideFiturItem}
-            >
-              <div className={styles.imageWrapper}>
-                <img src={item.image} alt={item.title} />
-                <div className={styles.titleOverlay}>{item.title}</div>
-              </div>
-            </a>
-          ),
-      )}
+    <div className={styles.guideFiturSection}>
+      <h2 className={styles.sectionTitle}>
+        Apa yang ingin Anda jelajahi terlebih dahulu?
+      </h2>
+      <div
+        className={classNames(styles.guideFiturWrap, {
+          [styles.threeMobile]: itemCount === 3,
+          [styles.fourMobile]: itemCount === 4,
+        })}
+      >
+        {guideItems.map((item, index) => (
+          <a key={index} href={item.url} className={styles.guideFiturItem}>
+            <div className={styles.imageWrapper}>
+              <img src={item.image} alt={item.title} />
+              <div className={styles.titleOverlay}>{item.title}</div>
+            </div>
+          </a>
+        ))}
+      </div>
     </div>
   )
 }
