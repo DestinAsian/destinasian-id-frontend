93179abe3bb94431a1fa6efbd1a7b8c30b3b3837
DestinAsian <webmaster@destinasian.com>
2025-07-11 17:16:00 +0700

    Update BannerPosterGuide layout: support landscape banner and responsive styles

--- components/BannerPosterGuide/BannerPosterGuide.js
+++ components/BannerPosterGuide/BannerPosterGuide.js
@@ -4,47 +4,48 @@ import classNames from 'classnames/bind'
 
 const cx = classNames.bind(styles)
 
-const BannerPosterGuide = ({ guideReelIg, bannerDa }) => {
-  if (!guideReelIg && !bannerDa) return null
+const BannerPosterGuide = ({ guideStorie }) => {
+  if (!guideStorie) return null
 
   const {
-    titleReelIg,
-    contentReelIg,
-    bannerReelIg2,
-    linkUrlBannerReelIg2,
-  } = guideReelIg || {}
-
-  const { linkBannerFokusHubDa, bannerFokusHubDa } = bannerDa || {}
+    bannerLandscape,
+    linkBannerLandscape,
+    bannerGuideStories,
+    linkBannerGuideStories,
+  } = guideStorie
 
   return (
     <div className={cx('wrapper')}>
       <div className={cx('contentGrid')}>
-        {/* Konten kiri: Reel IG */}
+        {/* Konten kiri: Banner Landscape */}
         <div className={cx('leftContent')}>
-          {bannerReelIg2?.mediaItemUrl && (
+          {bannerLandscape?.mediaItemUrl && (
             <div className={cx('leftBanner')}>
               <a
-                href={linkUrlBannerReelIg2}
+                href={linkBannerLandscape}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img src={bannerReelIg2.mediaItemUrl} alt="Banner Reel" />
+                <img
+                  src={bannerLandscape.mediaItemUrl}
+                  alt="Banner Landscape"
+                />
               </a>
             </div>
           )}
         </div>
 
-        {/* Konten kanan: Banner DA */}
+        {/* Konten kanan: Banner Guide Stories */}
         <div className={cx('rightBanner')}>
-          {bannerFokusHubDa?.mediaItemUrl && linkBannerFokusHubDa && (
+          {bannerGuideStories?.mediaItemUrl && linkBannerGuideStories && (
             <a
-              href={linkBannerFokusHubDa}
+              href={linkBannerGuideStories}
               target="_blank"
               rel="noopener noreferrer"
             >
               <img
-                src={bannerFokusHubDa.mediaItemUrl}
-                alt="Banner Fokus"
+                src={bannerGuideStories.mediaItemUrl}
+                alt="Banner Guide Stories"
                 className={cx('bannerImage')}
               />
             </a>
