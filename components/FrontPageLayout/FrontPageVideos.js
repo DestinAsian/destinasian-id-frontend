21990a954fd88d248950c951792963155a877b95
DestinAsian <webmaster@destinasian.com>
2025-07-14 14:29:38 +0700

    Add optimized FrontPageVideos and latest video only in ContentWrapperVideo

---
+++ components/FrontPageLayout/FrontPageVideos.js
@@ -0,0 +1,48 @@
+'use client'
+import React from 'react'
+import { useQuery } from '@apollo/client'
+import classNames from 'classnames/bind'
+import styles from './FrontPageVideos.module.scss'
+import dynamic from 'next/dynamic'
+import { GetVideos } from '../../queries/GetVideos'
+import Outnow from '../Outnow/Outnow'
+
+const ContentWrapperVideo = dynamic(() =>
+  import('../ContentWrapperVideo/ContentWrapperVideo'),
+)
+
+const cx = classNames.bind(styles)
+
+export default function FrontPageVideos() {
+  const { data, loading, error } = useQuery(GetVideos, {
+    variables: { first: 2 },
+    fetchPolicy: 'cache-and-network',
+    nextFetchPolicy: 'cache-first',
+  })
+
+  const videos = data?.videos?.edges || []
+
+  if (loading || error || videos.length === 0) return null
+
+  return (
+    <section className={cx('componentVideos')}>
+      <h2 className={cx('titleVideos')}>Videos</h2>
+      <div className={cx('newsUpdates')}>
+        <div className={cx('twoColumns')}>
+          <div className={cx('leftColumn')}>
+            <div className={cx('videoWrapper')}>
+              <ContentWrapperVideo data={videos} />
+            </div>
+          </div>
+          <div className={cx('rightColumn')}>
+            <aside className={cx('outnowWrapper')}>
+              <div className={cx('outnowVideos')}>
+                <Outnow />
+              </div>
+            </aside>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
