fb3a341c8bb7c1f25d4b21c6cabec9a59af6a3b5
DestinAsian <webmaster@destinasian.com>
2025-07-18 13:50:16 +0700

    feat(document): add _document.js to inject Google Ad Manager GPT script

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Load GPT.js dari Google Ad Manager satu kali secara global */}
+        <script
+          async
+          src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
