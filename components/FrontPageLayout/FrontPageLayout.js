f7fd57ccb2d50345c18217776107fd56a85b111c
DestinAsian <webmaster@destinasian.com>
2025-07-25 15:04:43 +0700

    feat(frontpage): update layout dan konten video homepage

--- components/FrontPageLayout/FrontPageLayout.js
+++ components/FrontPageLayout/FrontPageLayout.js
@@ -1,106 +1,51 @@
-// import React from 'react'
 import React, { useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client'
 import classNames from 'classnames/bind'
-import styles from './FrontPageLayout.module.scss'
 import dynamic from 'next/dynamic'
 import Link from 'next/link'
 
-// GraphQL Queries
+import styles from './FrontPageLayout.module.scss'
 import { GetCategoryUpdates } from '../../queries/GetCategoryUpdates'
 import { GetCategoryFeatures } from '../../queries/GetCategoryFeatures'
 import { GetChildrenTravelGuides } from '../../queries/GetChildrenTravelGuides'
 
-// Static Components
 import TravelGuideCategories from '../TravelGuideCategories/TravelGuideCategories'
 
 // Dynamic Imports
-// Dynamic Imports (dengan ssr: false)
 const Outnow = dynamic(() => import('../Outnow/Outnow'), { ssr: false })
-const CategoryUpdates = dynamic(
-  () => import('../CategoryUpdates/CategoryUpdates'),
-  { ssr: false },
-)
-const CategoryNewsUpdates = dynamic(
-  () => import('../CategoryNewsUpdates/CategoryNewsUpdates'),
-  { ssr: false },
-)
-const CategoryFeatures = dynamic(
-  () => import('../CategoryFeatures/CategoryFeatures'),
-  { ssr: false },
-)
-const PreviewHalfPage1 = dynamic(() =>
-  import('../../components/AdUnit/Preview/PreviewHalfPage1/PreviewHalfPage1'),
-)
-const MastHeadTop = dynamic(() =>
-  import('../../components/AdUnit/MastHeadTop/MastHeadTop'),
-)
-const MastHeadTopMobile = dynamic(() =>
-  import('../../components/AdUnit/MastHeadTopMobile/MastHeadTopMobile'),
-)
-const MastHeadBottom = dynamic(() =>
-  import('../../components/AdUnit/MastHeadBottom/MastHeadBottom'),
-)
-const MastHeadBottomMobile = dynamic(() =>
-  import('../../components/AdUnit/MastHeadBottomMobile/MastHeadBottomMobile'),
-)
-const PreviewMastHeadBottom = dynamic(() =>
-  import(
-    '../../components/AdUnit/Preview/PreviewMastHeadBottom/PreviewMastHeadBottom'
-  ),
-)
-const PreviewMastHeadBottomMobile = dynamic(() =>
-  import(
-    '../../components/AdUnit/Preview/PreviewMastHeadBottomMobile/PreviewMastHeadBottomMobile'
-  ),
-)
+const CategoryUpdates = dynamic(() => import('../CategoryUpdates/CategoryUpdates'), { ssr: false })
+const CategoryNewsUpdates = dynamic(() => import('../CategoryNewsUpdates/CategoryNewsUpdates'), { ssr: false })
+const CategoryFeatures = dynamic(() => import('../CategoryFeatures/CategoryFeatures'), { ssr: false })
+const HalfPageHome1 = dynamic(() => import('../AdUnit/HalfPage1/HalfPageHome1'))
+const MastHeadTopHome= dynamic(() => import('../AdUnit/MastHeadTop/MastHeadTopHome'))
+const MastHeadTopMobileHome = dynamic(() => import('../AdUnit/MastHeadTopMobile/MastHeadTopMobileHome'))
+const MastHeadBottomHome= dynamic(() => import('../AdUnit/MastHeadBottom/MastHeadBottomHome'))
+const MastHeadBottomMobileHome = dynamic(() => import('../AdUnit/MastHeadBottomMobile/MastHeadBottomMobileHome'))
 
 const cx = classNames.bind(styles)
 
 export default function FrontPageLayout() {
-  // Get Travel Guide Categories
-  const {
-    data: travelGuideData,
-    loading: travelGuideLoading,
-    error: travelGuideError,
-  } = useQuery(GetChildrenTravelGuides, {
+  const [isMobile, setIsMobile] = useState(false)
+
+  const { data: travelGuideData, loading: travelGuideLoading, error: travelGuideError } = useQuery(GetChildrenTravelGuides, {
     fetchPolicy: 'cache-first',
-    // nextFetchPolicy: 'cache-and-network',
   })
 
-  // Get Updates
-  const {
-    data: updatesData,
-    loading: updatesLoading,
-    error: updatesError,
-  } = useQuery(GetCategoryUpdates, {
+  const { data: updatesData, loading: updatesLoading, error: updatesError } = useQuery(GetCategoryUpdates, {
     variables: { include: ['41'] },
     fetchPolicy: 'cache-first',
-    // nextFetchPolicy: 'cache-and-network',
   })
 
-  // Get Features
-  const {
-    data: featuresData,
-    loading: featuresLoading,
-    error: featuresError,
-  } = useQuery(GetCategoryFeatures, {
+  const { data: featuresData, loading: featuresLoading, error: featuresError } = useQuery(GetCategoryFeatures, {
     variables: { id: '20' },
     fetchPolicy: 'cache-first',
-    // nextFetchPolicy: 'cache-and-network',
   })
 
-  // Prepare Data
   const categoryEdges = updatesData?.category?.children?.edges || []
   const categoryFeatures = featuresData?.category
 
-  const [isMobile, setIsMobile] = useState(false)
-
   useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768)
-    }
-
+    const handleResize = () => setIsMobile(window.innerWidth <= 768)
     handleResize()
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
@@ -108,17 +53,16 @@ export default function FrontPageLayout() {
 
   return (
     <>
-      <div>{isMobile ? <MastHeadTopMobile /> : <MastHeadTop />}</div>
-      {/* TRAVEL GUIDES CHILDREN */}
-      {!travelGuideLoading &&
-        !travelGuideError &&
-        travelGuideData?.category?.children?.edges?.length > 0 && (
-          <div className={cx('component-updates')}>
-            <div className={cx('category-insights-component')}>
-              <TravelGuideCategories data={travelGuideData} />
-            </div>
+      <div>{isMobile ? <MastHeadTopMobileHome /> : <MastHeadTopHome />}</div>
+
+      {/* TRAVEL GUIDE CHILDREN */}
+      {!travelGuideLoading && !travelGuideError && travelGuideData?.category?.children?.edges?.length > 0 && (
+        <div className={cx('component-updates')}>
+          <div className={cx('category-insights-component')}>
+            <TravelGuideCategories data={travelGuideData} />
           </div>
-        )}
+        </div>
+      )}
 
       <hr className={cx('divider')} />
 
@@ -130,29 +74,32 @@ export default function FrontPageLayout() {
           </div>
         </div>
       )}
-      {/* <div>{isMobile ? <MastHeadBottomMobile /> : <MastHeadBottom />}</div> */}
+
       <hr className={cx('divider')} />
 
       <div>
-        {isMobile ? <PreviewMastHeadBottomMobile /> : <PreviewMastHeadBottom />}
+        {isMobile ? <MastHeadBottomMobileHome/> : <MastHeadBottomHome />}
       </div>
-      <hr className={cx('divider')} />
-      {/* CATEGORY UPDATES - TITLE */}
-      {categoryEdges.map(({ node: category }) => {
-        const parentName = category?.parent?.node?.name || ''
-        const childName = category.name
-        return (
-          <div key={category.id} className={cx('category-updates-component')}>
-            <Link href={category.uri}>
-              <h2 className={styles.title}>
-                {parentName ? `${parentName} ${childName}` : childName}
-              </h2>
-            </Link>
-          </div>
-        )
-      })}
 
-      {/* CATEGORY NEWS UPDATES + OUTNOW */}
+      <hr className={cx('divider')} />
+
+      {/* CATEGORY UPDATES TITLES */}
+      {categoryEdges.length > 0 &&
+        categoryEdges.map(({ node: category }) => {
+          const parentName = category?.parent?.node?.name || ''
+          const childName = category.name
+          return (
+            <div key={category.id} className={cx('category-updates-component')}>
+              <Link href={category.uri}>
+                <h2 className={styles.title}>
+                  {parentName ? `${parentName} ${childName}` : childName}
+                </h2>
+              </Link>
+            </div>
+          )
+        })}
+
+      {/* CATEGORY NEWS UPDATES + HALF PAGE */}
       <div className={cx('component-news-updates')}>
         <div className={cx('two-columns')}>
           <div className={cx('left-column')}>
@@ -165,15 +112,14 @@ export default function FrontPageLayout() {
           <div className={cx('right-column')}>
             <aside className={cx('outnow-wrapper')}>
               <div className={cx('outnow-component')}>
-                {/* <Outnow /> */}
-                <PreviewHalfPage1 />
+                <HalfPageHome1 />
               </div>
             </aside>
           </div>
         </div>
       </div>
-      <hr className={cx('divider-news-updates')} />
 
+      <hr className={cx('divider-news-updates')} />
 
       {/* CATEGORY FEATURES */}
       {!featuresLoading && !featuresError && categoryFeatures && (
--- components/FrontPageLayout/FrontPageVideos.js
+++ components/FrontPageLayout/FrontPageVideos.js
@@ -1,27 +1,22 @@
 'use client'
+
 import React from 'react'
 import { useQuery } from '@apollo/client'
 import classNames from 'classnames/bind'
 import styles from './FrontPageVideos.module.scss'
 import dynamic from 'next/dynamic'
 import { GetVideos } from '../../queries/GetVideos'
-import Outnow from '../Outnow/Outnow'
 
-const ContentWrapperVideo = dynamic(
-  () => import('../ContentWrapperVideo/ContentWrapperVideo'),
-  { ssr: false },
-)
-const HalfPage2 = dynamic(() =>
-  import('../../components/AdUnit/HalfPage2/HalfPage2'),
-)
+// Import dinamis (lazy-load) untuk komponen berat
+const ContentWrapperVideo = dynamic(() => import('../ContentWrapperVideo/ContentWrapperVideo'), { ssr: false })
+const HalfPageHome2= dynamic(() => import('../../components/AdUnit/HalfPage2/HalfPageHome2'), { ssr: false })
 
 const cx = classNames.bind(styles)
 
 export default function FrontPageVideos() {
   const { data, loading, error } = useQuery(GetVideos, {
-    variables: { first: 2 },
-    fetchPolicy: 'cache-and-network',
-    nextFetchPolicy: 'cache-first',
+    variables: { first: 1 },
+    fetchPolicy: 'cache-first',
   })
 
   const videos = data?.videos?.edges || []
@@ -31,6 +26,7 @@ export default function FrontPageVideos() {
   return (
     <section className={cx('componentVideos')}>
       <h2 className={cx('titleVideos')}>Videos</h2>
+
       <div className={cx('newsUpdates')}>
         <div className={cx('twoColumns')}>
           <div className={cx('leftColumn')}>
@@ -38,15 +34,9 @@ export default function FrontPageVideos() {
               <ContentWrapperVideo data={videos} />
             </div>
           </div>
+          
           <div className={cx('rightColumn')}>
-            <HalfPage2 />
-
-            {/* <aside className={cx('outnowWrapper')}>
-              <div className={cx('outnowVideos')}>
-                <Outnow />
-                <HalfPage2 />
-              </div>
-            </aside> */}
+            <HalfPageHome2 />
           </div>
         </div>
       </div>
