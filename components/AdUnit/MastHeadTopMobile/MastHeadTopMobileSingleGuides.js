f1d7240e613b7264a74ccbaf3f5167b6aec87b96
DestinAsian <webmaster@destinasian.com>
2025-07-29 14:05:12 +0700

    Add new MastHeadTopMobileSingleGuides ad component for single guide mobile layout

---
+++ components/AdUnit/MastHeadTopMobile/MastHeadTopMobileSingleGuides.js
@@ -0,0 +1,23 @@
+import classNames from "classnames/bind";
+import styles from "./MastHeadTopMobileSingleGuides.module.scss";
+import { Ad } from "react-ad-manager";
+
+let cx = classNames.bind(styles);
+
+export default function MastHeadTopMobileSingleGuides() {
+  return (
+    <div className={cx("mobile-banner")}>
+      {/* MastHead Top Mobile */}
+      <div className={cx("masthead-banner")}>
+        <Ad
+          adUnit="/6808792/REVAMP_DAI_MASTHEAD_TOP_MOBILE_GUIDE"
+          name="div-gpt-ad-1753693385231-0"
+          size={[
+            [300, 250],
+            [320, 330],
+          ]}
+        />
+      </div>
+    </div>
+  );
+}
