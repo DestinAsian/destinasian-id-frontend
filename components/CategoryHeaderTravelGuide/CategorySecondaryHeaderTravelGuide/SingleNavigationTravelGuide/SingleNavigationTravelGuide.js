686861ca23205283a37bef566b5c81c0685d0778
DestinAsian <webmaster@destinasian.com>
2025-08-15 12:32:22 +0700

    modified-single-navigasi

--- components/CategoryHeaderTravelGuide/CategorySecondaryHeaderTravelGuide/SingleNavigationTravelGuide/SingleNavigationTravelGuide.js
+++ components/CategoryHeaderTravelGuide/CategorySecondaryHeaderTravelGuide/SingleNavigationTravelGuide/SingleNavigationTravelGuide.js
@@ -2,10 +2,8 @@ import classNames from 'classnames/bind'
 import styles from './SingleNavigationTravelGuide.module.scss'
 import { useQuery } from '@apollo/client'
 import { GetSingleNavigationTravelGuide } from '../../../../queries/GetSingleNavigationTravelGuide'
-
 import MainCategoryMenu from '../../../../components/TravelGuidesMenu/MainCategoryMenu/MainCategoryMenu'
 import TravelGuidesMenu from '../../../../components/TravelGuidesMenu/TravelGuidesMenu'
-import Link from 'next/link'
 
 let cx = classNames.bind(styles)
 
@@ -25,33 +23,28 @@ export default function SingleNavigation({
     nextFetchPolicy: 'cache-and-network',
   })
 
-  const parentNode =
-    data?.travelGuide?.categories?.edges?.[0]?.node?.parent?.node
+  const parentNode = data?.travelGuide?.categories?.edges?.[0]?.node?.parent?.node
   const children = parentNode?.children?.edges || []
 
   return (
-    <>
-      {/* Overlay blur di luar menu (klik untuk menutup) */}
-      {isNavShown && (
-        <div className={cx('blur-overlay')} onClick={() => setIsNavShown(false)} />
+    <div
+      className={cx(
+        'component',
+        isMainNavShown || isNavShown ? 'show' : undefined,
       )}
-
+    >
       <div
         className={cx(
-          'component',
+          'navbar-wrapper',
           isMainNavShown || isNavShown ? 'show' : undefined,
         )}
       >
-        <div
-          className={cx(
-            'navbar-wrapper',
-            isMainNavShown || isNavShown ? 'show' : undefined,
-          )}
-        >
-          {/* Tombol kiri: nama parent category (toggle menu Guides) */}
+        {/* Tombol Parent */}
+        {parentNode && (
           <div
             className={cx(
               isScrolled ? 'sticky-text-menu-wrapper' : 'text-menu-wrapper',
+              isNavShown ? 'show' : undefined,
             )}
           >
             <div className={cx('menu-button-single')}>
@@ -64,57 +57,55 @@ export default function SingleNavigation({
                 }}
               >
                 <div className={cx('da-guide-wrapper')}>
-                  <span className={cx('nav-name')}>
-                    {/* {'Guides'} */}
-                    {parentNode?.name ?? ''}
-                  </span>
+                  <span className={cx('nav-name')}>{parentNode.name}</span>
                 </div>
               </button>
             </div>
           </div>
+        )}
 
-          {/* List subkategori */}
-          <div className={cx('navigation-wrapper')}>
-            <ul className={cx('navigation')}>
-              {children.map(({ node }) => (
-                <li key={node?.uri} className={cx('nav-link')}>
-                  {node?.uri && (
-                    <Link
-                      href={node.uri}
-                      className={cx(
-                        isActiveCategory(node.uri) ? 'active' : 'not-active',
-                      )}
-                    >
-                      <h2 className={cx('nav-name')}>{node.name}</h2>
-                    </Link>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          {/* Menu Utama */}
-          <div
-            className={cx([
-              'full-menu-wrapper',
-              isMainNavShown ? 'show' : undefined,
-            ])}
-          >
-            <MainCategoryMenu categoryName={categoryName} />
-          </div>
-
-          {/* Menu Travel Guides */}
-          <div
-            className={cx([
-              'full-menu-wrapper',
-              'guides-box',
-              isNavShown ? 'show' : undefined,
-            ])}
-          >
-            <TravelGuidesMenu />
+        {/* List Subkategori */}
+        <div
+          className={cx(
+            'navigation-wrapper',
+            isMainNavShown || isNavShown ? 'show' : undefined,
+          )}
+        >
+          <div className={cx('navigation')}>
+            {children.map(({ node }) => (
+              <li key={node?.uri} className={cx('nav-link')}>
+                {node?.uri && (
+                  <a
+                    href={node.uri}
+                    className={cx(
+                      isActiveCategory(node.uri) ? 'active' : 'not-active',
+                    )}
+                  >
+                    <h2 className={cx('nav-name')}>{node.name}</h2>
+                  </a>
+                )}
+              </li>
+            ))}
           </div>
         </div>
+
+        {/* Menu Utama */}
+        <div
+          className={cx([
+            'full-menu-wrapper',
+            isMainNavShown ? 'show' : undefined,
+          ])}
+        >
+          <MainCategoryMenu categoryName={categoryName} />
+        </div>
+
+        {/* Menu Travel Guides */}
+        <div
+          className={cx(['full-menu-wrapper', isNavShown ? 'show' : undefined])}
+        >
+          <TravelGuidesMenu />
+        </div>
       </div>
-    </>
+    </div>
   )
 }
