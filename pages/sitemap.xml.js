867a7c245d4681dd7d0f7b16b23b03d41cd4207c
DestinAsian <webmaster@destinasian.com>
2025-09-19 17:47:59 +0700

    modified pages/sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,10 +1,31 @@
-// In pages/sitemap.xml.js:
+// // In pages/sitemap.xml.js:
+// import { getSitemapProps } from '@faustwp/core'
+
+// export default function Sitemap() {}
+
+// export function getServerSideProps(ctx) {
+//   return getSitemapProps(ctx, {
+//     frontendUrl: process.env.FRONTEND_URL,
+//   })
+// }
+
+
+// pages/sitemap.xml.js
 import { getSitemapProps } from '@faustwp/core'
 
-export default function Sitemap() {}
-
-export function getServerSideProps(ctx) {
-  return getSitemapProps(ctx, {
+export const getServerSideProps = async (ctx) => {
+  const sitemap = await getSitemapProps(ctx, {
     frontendUrl: process.env.FRONTEND_URL,
   })
+
+  ctx.res.setHeader('Content-Type', 'application/xml')
+  ctx.res.write(sitemap.props?.content || '')
+  ctx.res.end()
+
+  return { props: {} }
+}
+
+export default function Sitemap() {
+  // kosong, karena sudah di-handle di server response
+  return null
 }
