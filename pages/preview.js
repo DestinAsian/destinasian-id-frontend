8c2d0a705eea4aa74c36e02c34cb682890abfe76
DestinAsian <webmaster@destinasian.com>
2025-09-25 13:49:28 +0700

    Remove pages/api/faust/preview.js and update pages/preview.js

--- pages/preview.js
+++ pages/preview.js
@@ -1,12 +1,5 @@
-import { WordPressTemplate, getNextServerSideProps } from '@faustwp/core';
+import { WordPressTemplate } from '@faustwp/core';
 
 export default function Preview(props) {
   return <WordPressTemplate {...props} />;
-}
-
-export async function getServerSideProps(ctx) {
-  return getNextServerSideProps(ctx, {
-    Page: Preview,
-    props: {},
-  });
 }
