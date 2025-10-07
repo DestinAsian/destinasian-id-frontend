a321baf2338e9c4e160b27e8b1e64b75a5562339
DestinAsian <webmaster@destinasian.com>
2025-10-07 17:30:52 +0700

    modified-sitemap.xml

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,12 +1,59 @@
-// In pages/sitemap.xml.js:
-import { getSitemapProps } from '@faustwp/core'
+// // In pages/sitemap.xml.js:
+// import { getSitemapProps } from '@faustwp/core'
 
-export default function Sitemap() {}
+// export default function Sitemap() {}
 
-export function getServerSideProps(ctx) {
-  return getSitemapProps(ctx, {
-    frontendUrl: process.env.FRONTEND_URL,
-  })
+// export function getServerSideProps(ctx) {
+//   return getSitemapProps(ctx, {
+//     frontendUrl: process.env.FRONTEND_URL,
+//   })
+// }
+
+
+// pages/sitemap.xml.js
+
+export async function getServerSideProps({ res }) {
+  const frontendUrl =
+    process.env.FRONTEND_URL?.replace(/\/$/, '') || 'https://destinasian.co.id'
+  const remoteSitemapUrl = `${frontendUrl}/sitemap_index.xml`
+
+  try {
+    const response = await fetch(remoteSitemapUrl, {
+      headers: {
+        'User-Agent': 'Next.js Sitemap Proxy',
+        Accept: 'application/xml',
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error(`Gagal fetch sitemap (${response.status}) dari ${remoteSitemapUrl}`)
+    }
+
+    const xml = await response.text()
+
+    res.setHeader('Content-Type', 'text/xml')
+    res.write(xml)
+    res.end()
+  } catch {
+    const fallbackXml = `<?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+          <loc>${frontendUrl}/</loc>
+          <changefreq>daily</changefreq>
+          <priority>1.0</priority>
+        </url>
+      </urlset>`
+
+    res.setHeader('Content-Type', 'text/xml')
+    res.write(fallbackXml)
+    res.end()
+  }
+
+  return { props: {} }
+}
+
+export default function Sitemap() {
+  return null
 }
 
 
