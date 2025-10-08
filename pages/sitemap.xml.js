7fe90ce8d8b2d458dc33ddc1553d78e6f8db20dd
DestinAsian <webmaster@destinasian.com>
2025-10-08 11:50:52 +0700

    modified-sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -9,43 +9,45 @@
 //   })
 // }
 
-import fetch from 'node-fetch';
-
+// pages/sitemap.xml.js
 export async function getServerSideProps({ res }) {
-  const frontendUrl =
-    process.env.FRONTEND_URL?.replace(/\/$/, '') || 'https://destinasian.co.id';
-
   try {
-    const response = await fetch(`${frontendUrl}/sitemap.xml`);
-    if (!response.ok) throw new Error('Failed to fetch sitemap.xml');
-    const sitemapText = await response.text();
-
-    const locMatches = [...sitemapText.matchAll(/<loc>(.*?)<\/loc>/g)];
-    const sitemapList = locMatches.map((match) => ({
-      loc: match[1],
-      lastmod: new Date().toISOString(), // Bisa diganti sesuai kebutuhan
-    }));
+    const backendSitemaps = [
+      'author-sitemap.xml',
+      'category-sitemap.xml',
+      'contest-sitemap.xml',
+      'luxury-travel-sitemap.xml',
+      'page-sitemap.xml',
+      'post-sitemap.xml',
+      'post-sitemap2.xml',
+      'post-sitemap3.xml',
+      'post-sitemap4.xml',
+      'post-sitemap5.xml',
+      'post-sitemap6.xml',
+      'post-sitemap7.xml',
+      'travel-guide-sitemap.xml',
+    ];
 
     const sitemapIndexXml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${sitemapList
-  .map(
-    (item) => `
+${backendSitemaps
+      .map(
+        (fileName) => `
   <sitemap>
-    <loc>${item.loc}</loc>
-    <lastmod>${item.lastmod}</lastmod>
+    <loc>https://destinasian.co.id/sitemap/${fileName}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
   </sitemap>`
-  )
-  .join('')}
+      )
+      .join('')}
 </sitemapindex>`;
 
     res.setHeader('Content-Type', 'text/xml');
     res.write(sitemapIndexXml);
     res.end();
   } catch (error) {
-    console.error('Error generating sitemap:', error);
+    console.error('Error generating sitemap index:', error);
     res.statusCode = 500;
-    res.end('Error generating sitemap');
+    res.end('Error generating sitemap index');
   }
 
   return { props: {} };
