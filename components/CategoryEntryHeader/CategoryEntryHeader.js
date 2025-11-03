e8a711e0f30b48dca752f6b3ec499b155a4499d1
DestinAsian <webmaster@destinasian.com>
2025-11-03 17:16:35 +0700

    modified style category

--- components/CategoryEntryHeader/CategoryEntryHeader.js
+++ components/CategoryEntryHeader/CategoryEntryHeader.js
@@ -193,7 +193,16 @@ l961 -963 -961 -963 c-912 -913 -962 -965 -989 -1027 -40 -91 -46 -200 -15
   )
 
   return (
-    <div className={cx(['component', className])}>
+    // <div className={cx(['component', className])}>
+       <div
+    className={cx([
+      'component',
+      className,
+      {
+        'normal-category': isNormalCategory,
+        'guides-category': isGuide,
+      },
+    ])}>
       {isNormalCategory && (
         <Container>
           {/* Normal Category with Slider */}
