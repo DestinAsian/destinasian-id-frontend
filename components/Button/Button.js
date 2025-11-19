af23aabb96757615832c71e02784e1272b9a8869
DestinAsian <webmaster@destinasian.com>
2025-11-19 14:02:32 +0700

    modified button

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,15 +1,6 @@
 import Link from 'next/link'
 import styles from './Button.module.scss'
 
-/**
- * Button component, can render as <button> or <Link> if href is provided.
- *
- * @param {Object} props
- * @param {string} props.href Optional href, renders a Link if present
- * @param {'primary'|'secondary'} props.styleType Button style type
- * @param {string} props.className Optional additional className
- * @param {React.ReactNode} props.children Button content
- */
 export default function Button({ href, styleType = 'primary', className, children, ...props }) {
   const buttonClassName = [
     styles.button,
